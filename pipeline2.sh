6d20b04a1f7023cf12aaca14310ac7e6a9fa9cfd
RenzRoos <r.roos.2@umail.leidenuniv.nl>
2023-02-08 18:28:34 +0100

    pipe 2 & 3 done

---
+++ pipeline2.sh
@@ -0,0 +1,13 @@
+#Programmeer opdracht 1, data analysis pipelines
+#Door: Renz Roos, s2509652 
+
+#!/bin/sh
+
+#bestandsnaam meegegeven? 
+if [ -z "$1"  ]; then 
+    #error
+    echo "Geen file in aanroeping.\nRoep aan met \"./pipeline2.sh [FILE]\""
+else
+    #pipeline
+    bzcat $1 | grep 'Personenauto' | cut -d ',' -f  3,4 | sort | uniq -c | sort -n -r | head -10 | sed 's/^ *[0-9]* //'
+fi
