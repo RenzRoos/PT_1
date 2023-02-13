0d96b97a3414c1f7711c986082c25d5fc24ff739
RenzRoos <r.roos.2@umail.leidenuniv.nl>
2023-02-13 21:22:32 +0100

    remove tail

--- pipeline1.sh
+++ pipeline1.sh
@@ -9,5 +9,5 @@ if [ -z "$1"  ]; then
     echo "Geen file in aanroeping.\nRoep aan met \"./pipeline1.sh [FILE]\""
 else
     #pipeline
-    bzcat $1 | grep 'Personenauto' | cut -d ',' -f  1 | tail -n +2 | tr A-Z X | tr 0-9 9 | sort | uniq 
+    bzcat $1 | grep 'Personenauto' | cut -d ',' -f  1 | tr A-Z X | tr 0-9 9 | sort | uniq 
 fi
