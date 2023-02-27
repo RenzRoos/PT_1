9c7ce4d1129bfc3a8cc77b154da16e9c0355853f
RenzRoos <r.roos.2@umail.leidenuniv.nl>
2023-02-27 20:58:23 +0100

    pipeline 5 remove values

--- pipeline5_plot.py
+++ pipeline5_plot.py
@@ -12,10 +12,9 @@ count = pipeline5_dayCalc.dayCalc()
 
 #plot init
 days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-values = [count[0],count[1],count[2],count[3],count[4],count[5],count[6]]
 y_pos = np.arange(len(days))
 
-plt.bar(y_pos,values)
+plt.bar(y_pos,count)
 plt.xticks(y_pos, days)
 
 #plot labels
