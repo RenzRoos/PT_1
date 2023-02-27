c4524bb544a685ed7908f89c7c8d3ad08762ee97
RenzRoos <r.roos.2@umail.leidenuniv.nl>
2023-02-27 21:42:08 +0100

    clean up

--- pipeline6.sh
+++ pipeline6.sh
@@ -9,6 +9,6 @@ if [ -z "$1"  ]; then
     echo "Geen file in aanroeping.\nRoep aan met \"./pipeline6.sh [FILE]\""
 else
     #pipeline
-    bzcat $1 | awk -F "," '{if ($2 == "Personenauto" && ($24 != "" || $51="")) {print $3, $24, $51} }' | 
-    sort | awk '{ $(NF-1)=$(NF-1)","}1' | awk '{ $(NF-2)=$(NF-2)","}1' | python3 pipeline6_plot.py
+    bzcat $1 | awk -F "," '{if ($2 == "Personenauto" && ($24 != "" || $51="")) {print $3","$24","$51} }' | 
+    sort | python3 pipeline6_plot.py
 fi
